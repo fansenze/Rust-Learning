5af348693454c123ee3e4bf52a51d25eea89da6b
senze.fan <fansenze@songxiaocai.com>
2017-09-04 10:04:04 +0800

    init

---
+++ variables_mutability/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    const MAX_POINTS: u32 = 100_100;
+    println!("max_points is {}", MAX_POINTS);
+    let y = MAX_POINTS / 100;
+    println!("y is {}", y);
+    let mut x = 5;
+    println!("now x is {}", x);
+    x = 1111;
+    println!("then x is {}", x);
+}
