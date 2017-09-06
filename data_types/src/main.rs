2a2f153993941720e6139143f02c94f51be6aff1
senze.fan <fansenze@songxiaocai.com>
2017-09-06 12:12:41 +0800

    add README.md

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let num: i32 = "-42".parse().expect("Not a number!");
+    let num2: i32 = "42".parse().expect("Not a number!");
+    println!("{}, {}", num, num2);
 }
